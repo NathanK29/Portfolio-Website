872d0e221af457a43ed4ed705ee9a3c252e4029a
NathanK29 <aznathan29@gmail.com>
2024-01-08 15:40:07 -0700

    Changed layout/color scheme

--- src/Contact.js
+++ src/Contact.js
@@ -1,6 +1,6 @@
 import "./Contact.scss";
 import { Routes, Route } from "react-router-dom";
-import ContactLayout from "./components/Layout";
+import ContactLayout from "./components/Contact/Layout";
 
 function Contact() {
   return (
---
+++ src/components/Contact/Layout/index.js
@@ -0,0 +1,14 @@
+import "./index.scss";
+import TranslucentCard from "../TranslucentCard/translucentCard";
+import NavBar from "../../Home/NavBar/navbar"
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar />
+      <TranslucentCard />
+    </>
+  );
+};
+
+export default Layout;
