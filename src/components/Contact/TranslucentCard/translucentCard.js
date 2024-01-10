55a20fe89ec2aaa4e9398928a94a35665742fe84
NathanK29 <aznathan29@gmail.com>
2024-01-09 23:45:00 -0700

    icons on contact page as well as some style changes

--- src/components/Contact/TranslucentCard/translucentCard.js
+++ src/components/Contact/TranslucentCard/translucentCard.js
@@ -1,16 +1,28 @@
 import { Link } from "react-router-dom";
 import "./translucentCard.scss";
+import gitHubLogo from "./images/github.png";
+import linkedInLogo from "./images/linkedin.jpg";
+import gmailLogo from "./images/gmail.svg";
 
 const TranslucentCard = () => (
-  <div class="container">
-    <div class="card">
-      <div class="circle"></div>
-      <div class="circle"></div>
-      <div class="card-inner">
-        <h1 class="greeting">Contact</h1>
-        <h1 class="github">GitHub</h1>
-        <h1 class="linkedin">LinkedIn</h1>
-        <h1 class="email">Email</h1>
+  <div className="container">
+    <div className="card">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="card-inner">
+        <h1 className="greeting">Contact</h1>
+        <div className="icon">
+          <img src={gitHubLogo} alt="GitHub" />
+          <span className="text">github.com/dsds</span>
+        </div>
+        <div className="icon">
+          <img src={linkedInLogo} alt="LinkedIn" width="24" height="24" />
+          <span className="text">linkedin.com/dsds</span>
+        </div>
+        <div className="icon">
+          <img src={gmailLogo} alt="Gmail" width="24" height="24" />
+          <span className="text">aznathan29@gmail.com</span>
+        </div>
       </div>
     </div>
   </div>
